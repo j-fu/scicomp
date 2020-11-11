e14cb9c614e332ed9279459d2690c23e5849a96e
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2020-11-11 22:18:57 +0100

    nb04 video

--- pluto/nb04-julia-types.jl
+++ pluto/nb04-julia-types.jl
@@ -1,31 +1,14 @@
 ### A Pluto.jl notebook ###
-# v0.12.3
+# v0.12.9
 
 using Markdown
 using InteractiveUtils
-
-# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
-macro bind(def, element)
-    quote
-        local el = $(esc(element))
-        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
-        el
-    end
-end
 
 # ╔═╡ baf88246-01d1-11eb-3d35-1393445b1476
 using Pkg; Pkg.activate(mktempdir()); Pkg.add("PlutoUI"); using PlutoUI
 
 # ╔═╡ 1b70a5be-01d6-11eb-2cf0-9de8fa2a665e
 using LinearAlgebra,InteractiveUtils
-
-# ╔═╡ 0434bba0-01d2-11eb-124f-ef216f314163
-md"""Hide package status: $(@bind hide_pkg_status CheckBox(false))"""
-
-# ╔═╡ fccd863a-01d1-11eb-126c-f5a20a339413
-if !hide_pkg_status 
-	with_terminal(Pkg.status)
-end
 
 # ╔═╡ 5a1cb7c8-01d9-11eb-1bf2-33a6b84fc123
 Pkg.add("AbstractTrees")
@@ -149,14 +132,14 @@ test_dispatch(x::Int64)="special case Int64, x=$(x)";
 test_dispatch(3)
 
 # ╔═╡ 4c81312e-01d5-11eb-0fd8-3be89232486b
-test_dispatch(false)
+test_dispatch(TColor{Float16}(0.5,0.5,0.1))
 
 # ╔═╡ 625bfc6a-01d5-11eb-25cf-259a7943063d
 test_dispatch(3.0)
 
 # ╔═╡ 6d712526-01d5-11eb-3feb-e74c800fa893
 md"""
-Here we defined a generic method which works for any variable passed. In the case of `Int64` or `Float64` parameters, special cases are handeld by different methods of the same function. The compiler decides which method to call. This approach allows to specialize implemtations dependent on data types, e.g. in order to optimze perfomance.
+Here we defined a generic method which works for any variable passed. In the case of `Int64` or `Float64` parameters, special cases are handeld by different methods of the same function. The compiler decides which method to call. This approach allows to specialize implemtations dependent on data types, e.g. in order to optimize perfomance.
 
 The `methods` function can be used to figure out which methods of a function exists.
 """
@@ -199,7 +182,7 @@ md"""
    use the "duck test" — "If it walks like a duck and it quacks like a duck, then it must be a duck" —
    to determine if an object can be used for a particular purpose
 
-When trying to force a  variabl to have an abstract type,it
+- When trying to force a parmameter to have an abstract type,it
 ends up with having a conrete type which is compatible with that abstract type
 """
 
@@ -219,7 +202,7 @@ md"""
  - Types can have subtypes and a supertype
  - Concrete types are the leaves of the resulting type tree
  - Supertypes are necessarily abstract
- - There is only one supertype for every (abstract or concrete) type:
+ - There is only one supertype for every (abstract or concrete) type
  - Abstract types can have several subtypes
 """
 
@@ -255,7 +238,7 @@ AbstractTrees.Tree(Number)
 
 # ╔═╡ 6a7e7916-01da-11eb-2857-3fff96092457
 md"""
-There are operators for testin type relationships
+There are operators for testing type relationships
 """
 
 # ╔═╡ 76efc16e-01da-11eb-3a78-bd1c99b5c79e
@@ -263,6 +246,12 @@ There are operators for testin type relationships
 
 # ╔═╡ 7d5c58e6-01da-11eb-1c79-a7b650bf3dc4
  Float64<: Integer
+
+# ╔═╡ 3c243910-2463-11eb-1a37-8da1192a193e
+isa(3,Float64)
+
+# ╔═╡ 494294a0-2463-11eb-1fea-eff0f59ccfee
+isa(3.0,Float64)
 
 # ╔═╡ 8384fe94-01da-11eb-1c96-47c7cf183364
 md"""
@@ -294,13 +283,11 @@ md"""
 """
 
 # ╔═╡ Cell order:
-# ╟─baf88246-01d1-11eb-3d35-1393445b1476
-# ╟─0434bba0-01d2-11eb-124f-ef216f314163
-# ╟─fccd863a-01d1-11eb-126c-f5a20a339413
+# ╠═baf88246-01d1-11eb-3d35-1393445b1476
 # ╠═5a1cb7c8-01d9-11eb-1bf2-33a6b84fc123
 # ╠═1b70a5be-01d6-11eb-2cf0-9de8fa2a665e
 # ╠═87ee0ddc-01d9-11eb-24fc-0b4a10a7acc2
-# ╠═1b221e84-01d2-11eb-3e46-67e2a59b03dc
+# ╟─1b221e84-01d2-11eb-3e46-67e2a59b03dc
 # ╠═13ccfef0-01d3-11eb-0be7-87721fe2c996
 # ╠═3b84bb40-01d3-11eb-1416-db63210faba4
 # ╠═7462dc44-01d3-11eb-310e-3f6d6b6a7a4f
@@ -309,40 +296,42 @@ md"""
 # ╠═4c6a79ea-01d3-11eb-268e-c32f7a6c85cd
 # ╠═c924835e-01d3-11eb-0fcb-0d7d4050b32c
 # ╠═d2cca0be-01d3-11eb-017f-5f72d26f23ba
-# ╠═fb556f26-01d2-11eb-0244-d353d4ae0c9d
+# ╟─fb556f26-01d2-11eb-0244-d353d4ae0c9d
 # ╠═be8b20de-01d2-11eb-353e-2b2d81ca29b7
-# ╠═b0fd81fe-01d3-11eb-19da-190f2787d98a
+# ╟─b0fd81fe-01d3-11eb-19da-190f2787d98a
 # ╠═f61a4092-01d3-11eb-3460-5b62b5dae5de
 # ╠═17711ea0-01d4-11eb-2428-53f6e3d61d05
-# ╠═2eedb7dc-01d4-11eb-0cd6-c7ec9611e2bd
+# ╟─2eedb7dc-01d4-11eb-0cd6-c7ec9611e2bd
 # ╠═372503d0-01d4-11eb-1211-b9395834842a
 # ╠═4ef772e8-01d4-11eb-314a-ed0d1c24da77
-# ╠═c2be228a-01d4-11eb-243f-cf35c0ec0164
+# ╟─c2be228a-01d4-11eb-243f-cf35c0ec0164
 # ╠═f5cc25e6-0954-11eb-179b-eddff99dd392
 # ╠═0468c2da-0955-11eb-271b-5d84d5d8343d
 # ╠═0cc7808a-0955-11eb-0b4d-ff491af88cf5
 # ╠═125f7b0e-01d5-11eb-28ed-772426c25218
 # ╠═4c81312e-01d5-11eb-0fd8-3be89232486b
 # ╠═625bfc6a-01d5-11eb-25cf-259a7943063d
-# ╠═6d712526-01d5-11eb-3feb-e74c800fa893
+# ╟─6d712526-01d5-11eb-3feb-e74c800fa893
 # ╠═ec9abf90-01d5-11eb-257b-f3a2f681c7b9
-# ╠═ec5288ec-01d5-11eb-2b09-a96fbe8fc00f
+# ╟─ec5288ec-01d5-11eb-2b09-a96fbe8fc00f
 # ╠═c7194b4a-01d7-11eb-0175-fda4e2b3947a
 # ╠═450a16a6-01d8-11eb-0d07-979219c7e493
-# ╠═d776bf38-01d8-11eb-2765-43dc1dbc3344
+# ╟─d776bf38-01d8-11eb-2765-43dc1dbc3344
 # ╠═fd9463d4-01d8-11eb-0a87-f7924aa63bda
-# ╠═21d47428-01d9-11eb-1a5e-73de9310fc01
+# ╟─21d47428-01d9-11eb-1a5e-73de9310fc01
 # ╠═0ce950e0-01d9-11eb-0345-bd84b69b7e0a
-# ╠═49f559c2-01d9-11eb-3fb0-a9dc6c3ab515
+# ╟─49f559c2-01d9-11eb-3fb0-a9dc6c3ab515
 # ╠═327f7c32-01d9-11eb-0d80-69042308ae71
-# ╠═910831ea-01d9-11eb-359d-996096e2641c
+# ╟─910831ea-01d9-11eb-359d-996096e2641c
 # ╠═c66752b2-01d9-11eb-3c0e-cfe0e76253fd
 # ╠═ddf7a328-01d9-11eb-21b9-79c484f01134
-# ╠═6a7e7916-01da-11eb-2857-3fff96092457
+# ╟─6a7e7916-01da-11eb-2857-3fff96092457
 # ╠═76efc16e-01da-11eb-3a78-bd1c99b5c79e
 # ╠═7d5c58e6-01da-11eb-1c79-a7b650bf3dc4
-# ╠═8384fe94-01da-11eb-1c96-47c7cf183364
+# ╠═3c243910-2463-11eb-1a37-8da1192a193e
+# ╠═494294a0-2463-11eb-1fea-eff0f59ccfee
+# ╟─8384fe94-01da-11eb-1c96-47c7cf183364
 # ╠═8ef2b906-01da-11eb-1641-01ce9253d537
 # ╠═d3cdc5ca-01da-11eb-3c3b-2bb31eb037ee
 # ╠═da2d5e80-01da-11eb-1642-b519a74b2a87
-# ╠═f113e434-01da-11eb-15d2-edf4d0e13b20
+# ╟─f113e434-01da-11eb-15d2-edf4d0e13b20
